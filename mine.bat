d5e2e47dd23f2a9576b7a04928b98248fd188ed3
Patrick Higgins <quickpanic@me.com>
2014-02-15 23:22:43 -0800

    Much update
    added even more sweg

--- mine.bat
+++ mine.bat
@@ -3,6 +3,6 @@ ECHO Initialized
 PAUSE
 :loop
 PING 1.1.1.1 -n 1 -w 5000 >NUL
-ECHO Mining at 69 Gh/s
-ECHO Current GPU temperature - Over 9000C
+ECHO Mining at %RANDOM% kH/s
+ECHO Current GPU temperature (C) - %RANDOM%
 goto loop

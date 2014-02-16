3d35687623d0a58ea4fc9a644a8d0f16cdb91288
Ian10583 <ianguy2000@yahoo.com>
2014-02-15 23:33:12 -0800

    Update mine.bat

--- mine.bat
+++ mine.bat
@@ -6,4 +6,5 @@ PAUSE
 PING 1.1.1.1 -n 1 -w 5000 >NUL
 ECHO Mining at %RANDOM% kH/s
 ECHO Current GPU temperature (C) - %RANDOM%
+ECHO dickbutt.avi.jpeg.png.mp4\urmumsawh0ar.mps.ogg.trojan.rar 
 goto loop

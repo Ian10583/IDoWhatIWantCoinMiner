0576aa9e124050f80de803cc2b2ce66d287dd74e
Ian10583 <ianguy2000@yahoo.com>
2014-02-15 23:37:06 -0800

    Kool stuff added
    Multiple lines to help users understand what the program is doing.

--- mine.bat
+++ mine.bat
@@ -6,4 +6,10 @@ PAUSE
 PING 1.1.1.1 -n 1 -w 5000 >NUL
 ECHO Mining at %RANDOM% kH/s
 ECHO Current GPU temperature (C) - %RANDOM%
+ECHO Your mum is a wh0ar.
+ECHO Kill yourself.
+ECHO Mining isn't worth it.
+ECHO DOGE MUDAFUKA
+ECHO DATS WAYCIST
+ECHO TROJAN DETECTED! C:\users\ian\appdata\roaming\.minecraft\killyourself.avi.jpg.png.mp4.ogg
 goto loop
